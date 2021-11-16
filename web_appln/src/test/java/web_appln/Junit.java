d56f009f506f0da2896a2ad50c8c6d799b509e61
Jijikoshy88 <81867861+Jijikoshy88@users.noreply.github.com>
2021-11-16 18:30:44 +0530

    Add files via upload

---
+++ web_appln/src/test/java/web_appln/Junit.java
@@ -0,0 +1,15 @@
+package web_appln;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Junit {
+
+	@Test
+	public void test() {
+		System.out.println("Success");
+		fail("Not yet implemented");
+	}
+
+}
